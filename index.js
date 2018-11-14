6ca76e45679925fba8696964c6e6ea71dee2d97c
remarkable-workshop-2804 <jchaselubitz@gmail.com>
2018-11-14 19:19:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name) {
-  unshift.kittens(name)
+  kittens.unshift.(name)
   return kittens
 }
